b5a0ec2d00fa4401b1ff8543f4eae539432be365
tomas <tomas.gentile@gm2dev.com>
2024-10-17 10:16:34 -0300

    feat(script): add comments to explain the results

--- script.js
+++ script.js
@@ -21,6 +21,8 @@ console.time("forEach loop");
 array.forEach((element) => {});
 console.timeEnd("forEach loop");
 
+//Los loops a ejecuciones donde el mas rapido es el while y en otras el for, y el foreach es siempre por diferencia el mas lento.
+
 let globalNumber1 = 100;
 let globalNumber2 = 200;
 
@@ -43,6 +45,8 @@ for (let index = 0; index < 1000000; index++) {
 }
 console.timeEnd("local operation");
 
+//Las variables locales son mas rapidas que las globales, asumo que por el tiempo requerido para acceder a ellas en memoria.
+
 function getstringConcatenation(string1, string2) {
   return "This is a primitive concatenation  " + string1 + " " + string2;
 }
@@ -62,3 +66,5 @@ for (let index = 0; index < 1000000; index++) {
   getTemplateString("pedrito", "pedrito2");
 }
 console.timeEnd("template string");
+
+//Cuando se ejecuta una vez sola cada concatenacion, el template string es ligeramente mas rapido, pero al haber una iteracion, en el tiempo de ejecucion de cada uno se va haciendo mas grande esa diferencia.
